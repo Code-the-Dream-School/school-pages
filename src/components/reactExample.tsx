243edbf4b1ce4dacd897a18e844342ae1c66b710
Jack DeVries <jdevries3133@gmail.com>
2023-05-05 22:16:36 -0400

    feat: project boilerplate
    - astro
    - typescript
    - eslint
    - prettier
    - CI via github actions

---
+++ src/components/reactExample.tsx
@@ -0,0 +1,22 @@
+// This is an example where you can use react to define components
+
+import { useState } from "react";
+
+export const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="flex justify-center w-screen">
+      <div className="prose border-black border rounded p-4">
+        <p className="text-lg">A Little React</p>
+        <p className="text-sm">As a Snack</p>
+        <p>Count: {count}</p>
+        <button
+          className="bg-blue-200 p-2 rounded shadow"
+          onClick={() => setCount(count + 1)}
+        >
+          Increment Count
+        </button>
+      </div>
+    </div>
+  );
+};
