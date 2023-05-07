81a97d9742b1afab2abde88d2f04c5394d790544
Jack DeVries <jdevries3133@gmail.com>
2023-05-07 11:30:29 -0400

    feat: branded header

--- src/components/wip.tsx
+++ src/components/wip.tsx
@@ -1,7 +1,7 @@
 import { Link } from "./link";
 
 export const WipHeader = () => (
-  <header className="bg-yellow-100 w-full mb-2 text-sm italic">
+  <header className="bg-yellow-100 w-full text-sm italic">
     This site needs some love. Come contribute; we're open to contributors!{" "}
     <Link href="https://github.com/Code-the-Dream-School/school-pages">
       Come lend a hand!
