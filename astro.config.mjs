83e6b456a29da1056cb75ff711074e61bdae384f
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-09-19 18:01:45 -0400

    Add MDX integration so that we can import react components into Markdown files
    Note: I had to use an older version for peer dependency of the Astro that we're on

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import { ctdMermaidPlugin } from "./src/mermaidRemarkPlugin";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   markdown: {
     remarkPlugins: [ctdMermaidPlugin],
   },
